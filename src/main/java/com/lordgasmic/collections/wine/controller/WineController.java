bb2f62efc0edbd592e48772594de263acb8f0478
Nick Wellman <wellman.nicholas@gmail.com>
2022-01-26 22:40:59 -0500

    debug

--- src/main/java/com/lordgasmic/collections/wine/controller/WineController.java
+++ src/main/java/com/lordgasmic/collections/wine/controller/WineController.java
@@ -2,6 +2,7 @@ package com.lordgasmic.collections.wine.controller;
 
 import com.lordgasmic.collections.wine.models.WineryResponse;
 import com.lordgasmic.collections.wine.service.WineService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Slf4j
 public class WineController {
 
     @Autowired
@@ -31,6 +33,8 @@ public class WineController {
     @GetMapping("/api/v1/wines")
     public Object getWines(@RequestParam("wineId") final Optional<String> wineId,
                            @RequestParam("wineryId") final Optional<String> wineryId) throws SQLException {
+        log.info("wineid: " + wineId.get());
+        log.info("wineryId: " + wineryId.get());
         if (wineId.isEmpty() && wineryId.isEmpty()) {
             return wineService.getAllWines();
         } else if (wineId.isPresent()) {

0ab67a989083c5c29de1a0b33a38d3e6cb7c90c6
Nick Wellman <wellman.nicholas@gmail.com>
2022-01-26 22:49:58 -0500

    debug

--- src/main/java/com/lordgasmic/collections/wine/controller/WineController.java
+++ src/main/java/com/lordgasmic/collections/wine/controller/WineController.java
@@ -34,9 +34,9 @@ public class WineController {
     public Object getWines(@RequestParam("wineId") final Optional<String> wineId,
                            @RequestParam("wineryId") final Optional<String> wineryId) throws SQLException {
         log.info("wineid: " + wineId.get());
-        log.info("wineid is empty: " + wineId.isEmpty());
+        log.info("wineid is empty: " + wineId.get());
         log.info("wineryId: " + wineryId.get());
-        log.info("winerid: " + wineryId.isEmpty());
+        log.info("winerid: " + wineryId.get());
         if (wineId.isEmpty() && wineryId.isEmpty()) {
             return wineService.getAllWines();
         } else if (wineId.isPresent()) {

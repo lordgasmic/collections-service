b3a0f2725f2d6d208562bff81cf15d782c976818
Nick Wellman <wellman.nicholas@gmail.com>
2022-01-27 21:02:05 -0500

    add more sql

---
+++ src/main/java/com/lordgasmic/collections/MCP.java
@@ -0,0 +1,10 @@
+package com.lordgasmic.collections;
+
+import java.util.Optional;
+
+public class MCP {
+    public static void main(final String... args) {
+        System.out.println(Optional.empty().isEmpty());
+        System.out.println(Optional.empty().isEmpty());
+    }
+}

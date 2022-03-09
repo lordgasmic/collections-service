685d423964a5316fb8933a241e5b808bfa8aef02
Nick Wellman <wellman.nicholas@gmail.com>
2022-03-09 09:09:32 -0500

    test image uplaod

---
+++ src/main/java/com/lordgasmic/collections/wine/controller/WineImageController.java
@@ -0,0 +1,23 @@
+package com.lordgasmic.collections.wine.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@Slf4j
+public class WineImageController {
+
+    @PutMapping("/api/v1/wineImages")
+    public Object addWineImage(@RequestParam("imageFile") final MultipartFile imageFile) throws IOException {
+        log.info("Image Upload:");
+        log.info("Filename: " + imageFile.getOriginalFilename());
+        log.info(("File Bytes: " + imageFile.getBytes().length));
+
+        return "derp";
+    }
+}

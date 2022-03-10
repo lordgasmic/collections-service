14ca6ed86aecdd77c79b6a4f4f78b9e1847a4001
Nick Wellman <wellman.nicholas@gmail.com>
2022-03-10 10:29:25 -0500

    try to add some beans to the mix

--- src/main/java/com/lordgasmic/collections/wine/controller/WineImageController.java
+++ src/main/java/com/lordgasmic/collections/wine/controller/WineImageController.java
@@ -1,16 +1,32 @@
 package com.lordgasmic.collections.wine.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+import javax.servlet.MultipartConfigElement;
 import java.io.IOException;
 
 @RestController
 @Slf4j
 public class WineImageController {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        return new MultipartConfigElement("");
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        final CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(-1);
+        return multipartResolver;
+    }
 
     @PutMapping("/api/v1/wineImages")
     public Object addWineImage(@RequestParam("imageFile") final MultipartFile imageFile) throws IOException {

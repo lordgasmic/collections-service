866ed40a13cfeb217f7bb8a14678e500b910699b
Nick Wellman <wellman.nicholas@gmail.com>
2022-03-11 16:46:15 -0500

    process images

--- src/main/java/com/lordgasmic/collections/wine/service/WineImageService.java
+++ src/main/java/com/lordgasmic/collections/wine/service/WineImageService.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 import static java.util.stream.Collectors.toList;
 
@@ -23,18 +24,34 @@ public class WineImageService {
     private static final String REPO_NAME = "WineTastingRepository";
 
     private final GSARepository wineRepository;
+    private final ImageProcessor imageProcessor;
 
     public WineImageService() {
         wineRepository = (GSARepository) Nucleus.getInstance().getGenericService(REPO_NAME);
+        imageProcessor = new ImageProcessor();
     }
 
     public WineImageResponse addWineImage(final int wineId, final String label, final MultipartFile file) throws SQLException, IOException {
-        final MutableRepositoryItem item = wineRepository.createItem(WineImageConstants.ITEM_DESCRIPTOR_NAME);
+        final String shortMimeType = Objects.requireNonNull(file.getContentType()).substring(file.getContentType().indexOf('/') + 1);
+        log.info("resizing...");
+        final byte[] resizeBytes = imageProcessor.resizeImage(file.getBytes(), shortMimeType);
+        final MutableRepositoryItem item = wineRepository.createItem(WineImageConstants.WINE_IMAGE_ITEM_DESCRIPTOR_NAME);
         item.setProperty(WineImageConstants.PROPERTY_WINE_ID, wineId);
         item.setProperty(WineImageConstants.PROPERTY_LABEL, label);
-        item.setProperty(WineImageConstants.PROPERTY_IMAGE, file.getBytes());
+        item.setProperty(WineImageConstants.PROPERTY_IMAGE, resizeBytes);
         item.setProperty(WineImageConstants.PROPERTY_MIME_TYPE, file.getContentType());
+        log.info("adding image to repository");
         final RepositoryItem repositoryItem = wineRepository.addItem(item);
+
+        log.info("resizing thumbnail...");
+        final byte[] thumbnailBytes = imageProcessor.resizeImageToThumbnail(file.getBytes(), shortMimeType);
+        final MutableRepositoryItem thumbnailItem = wineRepository.createItem(WineImageConstants.WINE_IMAGE_THUMBNAIL_ITEM_DESCRIPTOR_NAME);
+        thumbnailItem.setProperty(WineImageConstants.PROPERTY_WINE_ID, wineId);
+        thumbnailItem.setProperty(WineImageConstants.PROPERTY_LABEL, label);
+        thumbnailItem.setProperty(WineImageConstants.PROPERTY_THUMBNAIL, thumbnailBytes);
+        thumbnailItem.setProperty(WineImageConstants.PROPERTY_MIME_TYPE, file.getContentType());
+        log.info("adding thumbnail to repository");
+        wineRepository.addItem(thumbnailItem);
 
         final WineImageResponse response = new WineImageResponse();
         response.setWineImages(List.of(convertRepositoryItemToWineImage(repositoryItem)));
@@ -42,11 +59,10 @@ public class WineImageService {
     }
 
     public WineImageResponse getWineImages(final int wineId) throws SQLException {
-        final List<RepositoryItem> items = wineRepository.getAllRepositoryItems(WineImageConstants.ITEM_DESCRIPTOR_NAME);
-        final List<WineImage> images = items.stream()
-                                            .filter(item -> item.getPropertyValue(WineImageConstants.PROPERTY_WINE_ID).equals(wineId))
-                                            .map(WineImageService::convertRepositoryItemToWineImage)
-                                            .collect(toList());
+        final List<RepositoryItem> items = wineRepository.getRepositoryItems(Integer.toString(wineId),
+                                                                             WineImageConstants.PROPERTY_WINE_ID,
+                                                                             WineImageConstants.WINE_IMAGE_ITEM_DESCRIPTOR_NAME);
+        final List<WineImage> images = items.stream().map(WineImageService::convertRepositoryItemToWineImage).collect(toList());
         final WineImageResponse response = new WineImageResponse();
         response.setWineImages(images);
         return response;

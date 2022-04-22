868a28a4eaa565a9fdea5ee35fb8065c1bafee1c
Nick Wellman <wellman.nicholas@gmail.com>
2022-04-22 12:15:02 -0400

    adding some rabbitmq

--- src/main/java/com/lordgasmic/collections/wine/config/RabbitMQConfig.java
+++ src/main/java/com/lordgasmic/collections/wine/config/RabbitMQConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,15 +39,15 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(queue).to(exchange).with(routingkey);
     }
 
-    //    @Bean
-    //    public MessageConverter jsonMessageConverter() {
-    //        return new Jackson2JsonMessageConverter();
-    //    }
-    //
-    //    @Bean
-    //    public AmqpTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-    //        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-    //        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-    //        return rabbitTemplate;
-    //    }
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
 }

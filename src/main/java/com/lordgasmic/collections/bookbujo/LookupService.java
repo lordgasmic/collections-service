5a521ac9e8eef953256d542c3b9cc990b9166359
Nick Wellman <wellman.nicholas@gmail.com>
2023-06-21 14:33:04 -0400

    update service to check that isbn is found

--- src/main/java/com/lordgasmic/collections/bookbujo/LookupService.java
+++ src/main/java/com/lordgasmic/collections/bookbujo/LookupService.java
@@ -2,7 +2,10 @@ package com.lordgasmic.collections.bookbujo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lordgasmic.collections.bookbujo.models.BookResponse;
+import com.lordgasmic.collections.bookbujo.models.Identifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,8 +17,8 @@ import java.net.http.HttpResponse;
 @Service
 public class LookupService {
 
-    private static final String BOOK_API_KEY="AIzaSyDqX9gYU-uEDSVfrLTURllDYQRzlw1xvNw";
-    private static final String BOOK_FIELDS="kind,items(volumeInfo/title,volumeInfo/authors,volumeInfo/pageCount,volumeInfo/categories,volumeInfo/industryIdentifiers)";
+    private static final String BOOK_API_KEY = "AIzaSyDqX9gYU-uEDSVfrLTURllDYQRzlw1xvNw";
+    private static final String BOOK_FIELDS = "kind,items(volumeInfo/title,volumeInfo/authors,volumeInfo/pageCount,volumeInfo/categories,volumeInfo/industryIdentifiers)";
 
 
     private final HttpClient client;
@@ -29,11 +32,23 @@ public class LookupService {
     public Object getBookDataByISBN(long isbn) throws URISyntaxException, IOException, InterruptedException {
         String sb = "q=isbn:" + isbn + "&" + "key=" + BOOK_API_KEY + "&" + "maxResults=1" + "&" + "fields=" + BOOK_FIELDS;
 
-        HttpRequest request = HttpRequest.newBuilder()
-                                         .uri(new URI("https://www.googleapis.com/books/v1/volumes?" + sb))
-                                         .GET()
-                                         .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()       );
-        return objectMapper.readValue(response.body(), BookResponse.class);
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://www.googleapis.com/books/v1/volumes?" + sb)).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        BookResponse bookResponse = objectMapper.readValue(response.body(), BookResponse.class);
+        boolean found = false;
+        for (Identifier identifier : bookResponse.getItems().get(0).getVolumeInfo().getIndustryIdentifiers()) {
+            if (identifier.getType().equals("ISBN_13")) {
+                long foundISBN = Long.parseLong(identifier.getIdentifier());
+                if (isbn != foundISBN) {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "isbn did not match");
+                }
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "unable to find isbn in api");
+        }
+        return bookResponse;
     }
 }

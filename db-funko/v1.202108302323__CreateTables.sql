56b1306443649bcc643f116ea379782fca6c2215
Nick Wellman <lordgasmic@lordgasmic.com>
2024-07-31 10:22:50 -0400

    remove funko

--- db-funko/v1.202108302323__CreateTables.sql
+++
@@ -1,6 +0,0 @@
-create table funko(
-    id int NOT NULL AUTO_INCREMENT,
-    fandom nvarchar(255) NOT NULL,
-    name nvarchar(255) NOT NULL,
-    CONSTRAINT funko_pk PRIMARY KEY (id)
-);
--- db-funko/v1.202108302324__CreateView.sql
+++
@@ -1,3 +0,0 @@
-create or replace view funko_vw as select * from funko;
-grant select, insert, update, delete on web.funko_vw to 'web_appl'@'localhost';
-grant select, insert, update, delete on web.funko_vw to 'web_appl'@'%';
